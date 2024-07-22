4b7a1574fcb3647d053e7c7f1ec640def6a85839
Michał Malec <michalmalec@MacBook-Pro-Micha.local>
2024-07-22 16:27:41 +0200

    feat - add deals_amount column to companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,7 @@
 class Company < ApplicationRecord
   has_many :deals
+
+  def update_deals_amount!
+    update_columns(deals_amount: deals.sum(:amount))
+  end
 end
--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,3 +1,11 @@
 class Deal < ApplicationRecord
   belongs_to :company
+
+  after_commit :update_company_deals_amount
+
+  private
+
+  def update_company_deals_amount
+    company.update_deals_amount!
+  end
 end
