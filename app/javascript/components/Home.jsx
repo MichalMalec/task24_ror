44b0f9b06dc334c938c4d6f110c85fcd91a0d55d
michal <michalmalec5@o2.pl>
2024-07-23 13:28:25 +0200

    feat - add fixed width

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -35,7 +35,7 @@ export default () => {
 
   return (
     <div className="vw-100 primary-color d-flex align-items-center justify-content-center">
-      <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="jumbotron fixed-width bg-transparent">
         <div className="container secondary-color">
           <h1 className="display-4">Companies</h1>
 
